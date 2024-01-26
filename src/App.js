91305cb68181e374001ea0139f1ffcd5f646aba1
Udhaya-Krishnan003 <udhayasathasivam2003@gmail.com>
2024-01-26 14:00:34 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import Layout from "./Layout";
-import Home from "./Home";
-import About from "./About";
-import Contact from "./Contact";
-export default function App(){
-  return(
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout/>}>
-        <Route index element={<Home/>}/>
-        <Route path="about" element={<About/>}/>
-        <Route path="contact" element={<Contact/>}/>
-      </Route>
-    </Routes>
-    </BrowserRouter>
+// import { BrowserRouter,Routes,Route } from "react-router-dom";
+// import Layout from "./Layout";
+// import Home from "./Home";
+// import About from "./About";
+// import Contact from "./Contact";
+// export default function App(){
+//   return(
+//     <BrowserRouter>
+//     <Routes>
+//       <Route path="/" element={<Layout/>}>
+//         <Route index element={<Home/>}/>
+//         <Route path="about" element={<About/>}/>
+//         <Route path="contact" element={<Contact/>}/>
+//       </Route>
+//     </Routes>
+//     </BrowserRouter>
+//   )
+// }
+import React from 'react'
+import Counter from './Counter'
+
+export default function App() {
+  return (
+    <div><Counter/></div>
   )
 }
